78a778d92090b9d8ed772d66bc6375e6ff597c58
jinj00oo <icems0428@naver.com>
2026-04-02 22:28:31 +0900

    chore: prettier 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="flex flex-1 w-full flex-col items-center justify-center">empty page</main>
+      <main className="flex flex-1 w-full flex-col items-center justify-center">
+        empty page
+      </main>
     </div>
   );
 }
