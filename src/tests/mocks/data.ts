96e9b0118d81ebefdb25f6fff9822946560b937f
jinj00oo <icems0428@naver.com>
2026-06-03 17:41:00 +0900

    chore: modify mapper, mock data type

--- src/tests/mocks/data.ts
+++ src/tests/mocks/data.ts
@@ -1,20 +1,20 @@
 import { NodeDTO } from "@/src/entities/tree/api/types";
 
 export const RAW_TREE_DATA: NodeDTO[] = [
-  { node_id: 1, title: "node 1", memo: null, parent_id: null, order_id: 0 },
-  { node_id: 2, title: "node 2", memo: null, parent_id: 1, order_id: 1 },
-  { node_id: 3, title: "node 3", memo: null, parent_id: 1, order_id: 6 },
-  { node_id: 21, title: "node 2a", memo: null, parent_id: 2, order_id: 2 },
-  { node_id: 22, title: "node 2b", memo: null, parent_id: 2, order_id: 3 },
-  { node_id: 23, title: "node 2c", memo: null, parent_id: 2, order_id: 4 },
-  { node_id: 24, title: "node 2d", memo: null, parent_id: 23, order_id: 5 },
-  { node_id: 4, title: "node 4", memo: null, parent_id: null, order_id: 7 },
-  { node_id: 5, title: "node 5", memo: null, parent_id: 4, order_id: 8 },
-  { node_id: 6, title: "output", memo: null, parent_id: 5, order_id: 9 },
-  { node_id: 7, title: "output", memo: null, parent_id: 5, order_id: 10 },
+  { nodeId: 1, name: "node 1", memo: null, parentId: null, orderId: 0 },
+  { nodeId: 2, name: "node 2", memo: null, parentId: 1, orderId: 1 },
+  { nodeId: 3, name: "node 3", memo: null, parentId: 1, orderId: 6 },
+  { nodeId: 21, name: "node 2a", memo: null, parentId: 2, orderId: 2 },
+  { nodeId: 22, name: "node 2b", memo: null, parentId: 2, orderId: 3 },
+  { nodeId: 23, name: "node 2c", memo: null, parentId: 2, orderId: 4 },
+  { nodeId: 24, name: "node 2d", memo: null, parentId: 23, orderId: 5 },
+  { nodeId: 4, name: "node 4", memo: null, parentId: null, orderId: 7 },
+  { nodeId: 5, name: "node 5", memo: null, parentId: 4, orderId: 8 },
+  { nodeId: 6, name: "output", memo: null, parentId: 5, orderId: 9 },
+  { nodeId: 7, name: "output", memo: null, parentId: 5, orderId: 10 },
 ];
 
 export const RAW_TREE_DATA_WITH_CYCLE: NodeDTO[] = [
-  { node_id: 100, title: "Cycle 1", memo: null, parent_id: 101, order_id: 1 },
-  { node_id: 101, title: "Cycle 2", memo: null, parent_id: 100, order_id: 2 },
+  { nodeId: 100, name: "Cycle 1", memo: null, parentId: 101, orderId: 1 },
+  { nodeId: 101, name: "Cycle 2", memo: null, parentId: 100, orderId: 2 },
 ];
