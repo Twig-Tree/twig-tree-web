3f80a18bd35a806c1bdf373b6b2a0fabc0acfb6e
jinj00oo <icems0428@naver.com>
2026-07-01 15:16:49 +0900

    refactor: useinitialize 커스텀 훅 분리

--- app/workspace/[workspaceId]/page.tsx
+++ app/workspace/[workspaceId]/page.tsx
@@ -15,24 +15,23 @@ import {
 } from "@/src/features/tree-editor/model/treeStore";
 import { useEffect } from "react";
 import { useGetTreeQuery } from "@/src/entities/tree/model/queries";
-import {
-  mapToVisualNodes,
-  mapToVisualEdges,
-} from "@/src/features/tree-editor/lib/mappers";
-import { mapNodesDtoToDomain } from "@/src/entities/tree/lib/mappers";
 import { useReactFlowStoreSetters } from "@/src/features/tree-editor/hooks/useReactFlowStoreSetters";
+import { useInitializeTree } from "@/src/features/tree-editor/hooks/useInitializeTree";
 
 function LayoutFlow() {
-  // todo: treeId 동적 처리
+  const treeId = "1"; // todo: treeId 동적 처리
   // todo: React Server Component 사용
-  const { data: treeData, isLoading, isError } = useGetTreeQuery("1");
+  const {
+    data: treeData,
+    isLoading,
+    isError: isGetTreeError,
+  } = useGetTreeQuery(treeId);
 
   // Zustand 스토어에서 상태(State) 구독
   const nodes = useTreeStore((state) => state.nodes);
   const edges = useTreeStore((state) => state.edges);
 
   // Zustand 스토어에서 액션(Actions) 구독
-  const initializeTree = useTreeStore((state) => state.initializeTree);
   const onNodesChange = useTreeStore((state) => state.onNodesChange);
   const onEdgesChange = useTreeStore((state) => state.onEdgesChange);
   const onConnect = useTreeStore((state) => state.onConnect);
@@ -50,21 +49,12 @@ function LayoutFlow() {
     pause();
   }, [pause]);
 
-  useEffect(() => {
-    if (!treeData) return;
+  useInitializeTree({
+    treeId,
+    treeData,
+    clear,
+  });
 
-    const domainNodes = mapNodesDtoToDomain(treeData);
-    const visualNodes = mapToVisualNodes(domainNodes);
-    const visualEdges = mapToVisualEdges(domainNodes);
-
-    initializeTree({
-      treeId: "1", // todo: treeId 동적 처리
-      nodes: visualNodes,
-      edges: visualEdges,
-    });
-
-    clear();
-  }, [treeData, initializeTree, clear]);
 
   // 2. React Flow가 초기 노드들의 뷰포트 정렬(fitView)까지 마쳤을 때 히스토리 기록 재개
   const handleInit = () => {
