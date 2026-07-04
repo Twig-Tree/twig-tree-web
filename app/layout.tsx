5c0c3914b90456cc464e49c6ce46e56692b62d3d
jinj00oo <icems0428@naver.com>
2026-07-05 00:36:06 +0900

    chore: tanstack query devtools 설치 및 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { QueryProvider } from "@/src/shared/lib/react-query/QueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,10 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`h-full`}>
       <body className="min-h-full flex flex-col">
-        <QueryProvider>{children}</QueryProvider>
+        <QueryProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryProvider>
       </body>
     </html>
   );
