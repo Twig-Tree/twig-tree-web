04af73fad6f1cea46bff3ccc0fedcbebb6137070
jinj00oo <icems0428@naver.com>
2026-07-21 18:55:30 +0900

    chore: 폴더 쿼리키 수정

---
+++ src/entities/folder/model/queries.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { folderApi } from "../api/folderApi";
+import { folderQueryKeys } from "./queryKeys";
+
+export function useGetFolderListQuery(folderParentId: string | null) {
+  return useQuery({
+    queryKey: folderQueryKeys.childrenByParent(folderParentId),
+    queryFn: () => folderApi.getFolderList(folderParentId),
+  });
+}
